fb3c22a1768d8d083a76324f8ca4160e3ed17715
efwoods <efwoods@g.clemson.edu>
2018-04-13 20:20:39 -0700

    updated rotate.py for more robust automation

--- Preprocessing_and_Enrichment/py_scripts/rotate.py
+++ Preprocessing_and_Enrichment/py_scripts/rotate.py
@@ -1,14 +1,54 @@
 # Input: Void
 # Output: a rotated image
 
-import sys, os, shutil, zipfile
+import sys, os, shutil, zipfile, json, argparse
 
 # Import Pillow:
 from PIL import Image
 
-workingDirectory = '/home/efwoods/watson/Watson-Precision-Agriculture/Preprocessing_and_Enrichment/Data/Flight 1/Spirea/Healthy'
+# function definitions
+def rotate(filename, finalDirectory):
+	img = Image.open(filename)
+	shutil.copy(filename, finalDirectory) # positiveTrainingDirectory/negative/test
+	for x in range(5, 360, 5):
+		img2 = img.rotate(x)
+		extension = len(filename) - 4	# the number of characters minus the length of the extension
+		truncated_filename = filename[:extension]	# the truncated name of the file
+		saveName = truncated_filename + '_rotation_degree_' + str(x) + '.png'
+		img2.save(saveName)
+		shutil.move(saveName, finalDirectory)
 
-savingDirectory = '/home/efwoods/watson/Watson-Precision-Agriculture/Preprocessing_and_Enrichment/Data/Flight 1/Spirea/Rotated_Cropped_Healthy_Spirea'
+# first pass positiveTrainingDirectory with positive split_count then negativeTrainingDirectory
+def process(workDirectory, trainingDirectory, testDirectory, split_count):	
+	count = 0
+	for folderName, subfolders, filenames in os.walk(workDirectory):
+		print('The current folder is ' + folderName)
+	for subfolder in subfolders:
+		print('SUBFOLDER OF ' + folderName + ': ' + subfolder)
+	for filename in filenames:
+		print('FILE INSIDE ' + folderName + ': ' + filename)
+		if count < split_count:
+			rotate(filename, trainingDirectory)
+			count += 1		
+		else:
+			rotate(filename, testDirectory)
+'''	
+			with open('truth.json', 'w') as outfile:
+			    json.dump({
+			    "filename":filename,
+			    "class": args.c,
+			    "type": args.type,
+			    "score": '-1' 
+			     }, outfile)
+'''
+	print('')
+
+
+#print(args)
+
+#workingDirectory = '/home/efwoods/watson/Watson-Precision-Agriculture/Preprocessing_and_Enrichment/Data/Flight 1/Spirea/Healthy
+
+#savingDirectory = '/home/efwoods/watson/Watson-Precision-Agriculture/Preprocessing_and_Enrichment/Data/Flight 1/Spirea/Rotated_Cropped_Healthy_Spirea'
 
 # walks through the folder of the current working directory, then each subfolder, then each file. 
 
@@ -17,34 +57,59 @@ savingDirectory = '/home/efwoods/watson/Watson-Precision-Agriculture/Preprocessi
 #This results in 72 images including the original file. 
 
 #These images are saved and moved to a new location before the new images are zipped.
-count = 0
-os.mkdir(savingDirectory)
-os.chdir(workingDirectory)
-for folderName, subfolders, filenames in os.walk(workingDirectory):
-	print('The current folder is ' + folderName)
 
-	for subfolder in subfolders:
-		print('SUBFOLDER OF ' + folderName + ': ' + subfolder)
-	for filename in filenames:
-		print('FILE INSIDE ' + folderName + ': ' + filename)
-		number_of_images = len(os.listdir(workingDirectory)) - 1
-		if count < number_of_images:
-			count += 1
-			img = Image.open(filename)
-			for x in range(5, 360, 5):
-				img2 = img.rotate(x)
-				extension = len(filename) - 4	# the number of characters minus the length of the extension
-				truncated_filename = filename[:extension]	# the truncated name of the file
-				saveName = truncated_filename + '_rotation_degree_' + str(x) + '.png'
-				img2.save(saveName)
-				shutil.move(saveName, savingDirectory)
-			shutil.copy(filename, savingDirectory)
-	print('')
+#os.mkdir(args.sD)
 
-import json
-with open('data.json', 'w') as outfile:
-    json.dump({
-    "age":100,
-    "name":"mkyong.com",
-    "messages":["msg 1","msg 2","msg 3"]
-     }, outfile)
+# parse arguements
+parser = argparse.ArgumentParser(description='Curate a set of data for Modeling and Validation')
+parser.add_argument('--p', help='Directory to the POSITIVE examples', type=str, required=True)
+parser.add_argument('--n', help='Directory to the NEGATIVE examples', type=str, required=True)
+parser.add_argument('--s', help='SPLITS the data. Accepts an int between 0 and 100. This value indicates the percentage of images to be used as TRAINING images.', type=int, required=True)
+parser.add_argument('--d', help='Final DESTINATION directory. Expected to be located in Modeling and Validation', type=str, required=True)
+args = parser.parse_args()
+
+# validate that user argument input is acceptable
+
+
+positive_image_total = len(os.listdir(args.p))	# identify the total number of images in the directory
+negative_image_total = len(os.listdir(args.n))
+percent = float(args.s)/100 # calculate the percentage of images to be used as positive examples
+positive_count = int(positive_image_total * percent) # the integer number of images that will be used as positive examples
+negative_count = int(negative_image_total * percent)
+if ((positive_count == 0) || (positive_count == positive_image_total) || (negative_count == 0) || (negative_count == negative_image_total): # positive_count must be between 0 and the total number of images in the file to leave data for the test data
+	print("Error incorrect split. Please choose a different split.")
+	sys.exit(1) 	
+else:
+	# create a folder of processed data with three subfolders: negative examples, positive examples, test images made of negative and positive images
+	locate = "/" # may need to make /Flight
+	baseDirectory = args.p[:args.p.rfind(locate)] 
+	targetName = args.p[args.p.rfind(locate)+1:]
+	parentDirectory = baseDirectory + "/"+ targetName + "_processed" + "_" + percent + "%split"
+	positiveTrainingDirectory = parentDirectory + "/PostiveTrainingData" + "_" + targetName
+	negativeTrainingDirectory = parentDirectory + "/NegativeTrainingData" + "_" + targetName
+	testingDirectory = parentDirectory + "/TestData" + "_" + targetName
+	
+	os.mkdir(parentDirectory)
+	os.mkdir(positiveTrainingDirectory)
+	os.mkdir(negativeTrainingDirectory)
+	os.mkdir(testingDirectory)
+
+	process(args.p, positiveTrainingDirectory, testingDirectory, positive_count)
+	process(args.n, negativeTrainingDirectory, testingDirectory, negative_count)
+
+	# zip folders for Modeling and Validation
+	os.chdir(parentDirectory)
+	shutil.make_archive(positiveTrainingDirectory, 'zip')
+	shutil.make_archive(negativeTrainingDirectory, 'zip')
+	shutil.make_archive(testingDirectory, 'zip')
+	src = parentDirectory
+	dst = args.d + "/"+ targetName + "_processed" + "_" + percent + "%split"
+	shutil.copytree(src, dst, symlinks=False, ignore=None)
+	shutil.rmtree(src)
+
+#workingDirectory = args.p
+#os.chdir(workingDirectory)	# move to the workingDirectory
+
+#plt.show
+
+
--- Preprocessing_and_Enrichment/py_scripts/truth.py
+++ Preprocessing_and_Enrichment/py_scripts/truth.py
@@ -3,5 +3,7 @@ import json
 with open('truth_labels.json', 'w+') as outfile:
     json.dump({
     "filename": "12_Flight 1_Spirea_Healthy.png",
-    "class":"Healthy"
+    "class": "0",
+    "type": "test",
+    "score": "0.863"
      }, outfile)
